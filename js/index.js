4a09c2890e6686f57bac0e1ce1fc2cb56a66859d
dannyd216 <dparrish216@gmail.com>
2020-04-07 00:12:38 -0600

    all sections done. footer left

--- js/index.js
+++ js/index.js
@@ -73,7 +73,7 @@ function navBar() {
 navBar();
 
 
-/**CTA BUTTON & TEXT */
+/**CTA BUTTON & TEXT*/
 
 
 /**DOM TEXT */
@@ -85,6 +85,141 @@ ctaH1s.textContent = siteContent['cta']['h1'];
 
 
 /**BUTTON */
+
+//CREATE VAR AND ASSIGN IT TO QUERYSELECTOR
+const ctaButton = document.querySelector('button');
+
+
+//IT WORKS! THE TEXT IS IN THE BOX
+ctaButton.textContent = siteContent['cta']['button'];
+
+
+
+/**CTA CIRCLE IMG */
+
+// CREATE THE VAR FOR THE IMG
+
+// CREATE VAR FOR IMAGE USING THE SELECTOR
+const ctaImg = document.querySelector('#cta-img');
+
+
+// console.log(ctaImg);
+
+
+//GIVE THE IMAGE VAR A VALUE
+ctaImg.src = siteContent['cta']['img-src'];
+
+
+
+/**MAIN CONTENT SECTION*/
+
+
+/**FEATURES */
+
+//THIS CALLS UPON THE TOP-CONTENT CLASS AND THE CHILDREN TEXT-CONTENT
+const mainContentTitle = document.querySelectorAll('.text-content h4'); // grabs the main content text-content  h4 tags
+
+//GRABS THE MAIN CONTENT P TAGS
+const mainContentParag = document.querySelectorAll('.text-content p');
+
+// console.log(mainContentParag);
+
+
+
+// DUE TO QUERYSELECTORALL BEING A NODE WE CAN ACCESS ITS INDICES AND ASSIGN THEM
+
+// GRAB THE HEADING TAGS
+mainContentTitle[0].textContent = siteContent['main-content']['features-h4'];
+
+// STYLE THE HEADING TAGS
+mainContentTitle[0].style.fontWeight = 'bold';
+
+// GRAB THE PARAG CONTENT
+mainContentParag[0].textContent = siteContent['main-content']['features-content'];
+
+// console.log(mainContentTitle[0]);
+
+/**ABOUT SECTION */
+
+// GRAB THE HEADING TAGS
+mainContentTitle[1].textContent = siteContent['main-content']['about-h4'];
+
+// STYLE THE HEADING TAGS
+mainContentTitle[1].style.fontWeight = 'bold';
+
+// console.log(mainContentTitle[1]);
+
+// GRAB THE PARAG CONTENT
+mainContentParag[1].textContent = siteContent['main-content']['about-content'];
+
+/**SERVICES SECTION */
+
+// GRAB THE HEADING TAGS
+mainContentTitle[2].textContent = siteContent['main-content']['services-h4'];
+
+// STYLE THE HEADING TAGS
+mainContentTitle[2].style.fontWeight = 'bold';
+
+// console.log(mainContentTitle[2]);
+
+// GRAB THE PARAG CONTENT
+mainContentParag[2].textContent = siteContent['main-content']['services-content'];
+
+
+/**PRODUCT SECTION */
+
+// GRAB THE HEADING TAGS
+mainContentTitle[3].textContent = siteContent['main-content']['product-h4'];
+
+// STYLE THE HEADING TAGS
+mainContentTitle[3].style.fontWeight = 'bold';
+
+// console.log(mainContentTitle[3]);
+
+// GRAB THE PARAG CONTENT
+mainContentParag[3].textContent = siteContent['main-content']['product-content'];
+
+/**VISION SECTION */
+
+// GRAB THE HEADING TAGS
+mainContentTitle[4].textContent = siteContent['main-content']['vision-h4'];
+
+// STYLE THE HEADING TAGS
+mainContentTitle[4].style.fontWeight = 'bold';
+
+// console.log(mainContentTitle[4]);
+
+// GRAB THE PARAG CONTENT
+mainContentParag[4].textContent = siteContent['main-content']['vision-content'];
+
+/**MIDDLE IMAGE SECTION */
+
+// GRAB THE IMG SELECTOR
+const midImg = document.querySelector('.middle-img');
+
+midImg.src = siteContent['main-content']['middle-img-src'];
+
+// console.log(midImg);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
